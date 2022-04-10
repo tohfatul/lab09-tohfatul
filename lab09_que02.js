c4d2278bfcb2b0f797b7348c4ae087f28387c014
tohfatul <tohfatul@users.noreply.github.com>
2022-04-09 23:47:35 -0500

    Update lab09_que02.js

--- lab09_que02.js
+++ lab09_que02.js
@@ -21,7 +21,7 @@
 
         //check if student's answer is correct
         checkAnswer(correctAnswer) {
-            returncorrectAnswer.answer === this.answers;
+            return correctAnswer.answer === this.answers;
         }
     }
 
@@ -34,7 +34,7 @@
 
         //computing quiz score for given student
         scoreStudentBySid(sid) {
-            letans = 0;
+            let ans = 0;
             this.students
                 .find((x) => x.studentId === sid)
                 .answers.forEach((x) => {
@@ -44,46 +44,46 @@
                         }
                     });
                 });
-            returnans;
+            return ans;
         }
 
         // computing average score
         getAverageScore() {
-            letsum = 0;
+            let sum = 0;
             this.students.forEach(x => {
                 sum += this.scoreStudentBySid(x.studentId)
             });
 
-            returnsum / this.students.length;
+            return sum / this.students.length;
         }
     }
 
-    conststudent1 = newStudent(10);
-    student1.addAnswer(newQuestion(2, "a"));
-    student1.addAnswer(newQuestion(3, "b"));
-    student1.addAnswer(newQuestion(1, "b"));
+    const student1 = new Student(10);
+    student1.addAnswer(new Question(2, "a"));
+    student1.addAnswer(new Question(3, "b"));
+    student1.addAnswer(new Question(1, "b"));
 
-    conststudent2 = newStudent(11);
-    student2.addAnswer(newQuestion(3, "b"));
-    student2.addAnswer(newQuestion(2, "a"));
-    student2.addAnswer(newQuestion(1, "d"));
+    const student2 = new Student(11);
+    student2.addAnswer(new Question(3, "b"));
+    student2.addAnswer(new Question(2, "a"));
+    student2.addAnswer(new Question(1, "d"));
 
-    conststudents = [student1, student2];
+    const students = [student1, student2];
 
-    constquestions = [
-        newQuestion(1, "b"),
-        newQuestion(2, "a"),
-        newQuestion(3, "b"),
+    const questions = [
+        new Question(1, "b"),
+        new Question(2, "a"),
+        new Question(3, "b"),
     ];
 
-    constquiz = newQuiz(questions, students);
-    letscoreforStudent10 = quiz.scoreStudentBySid(10);
+    const quiz = new Quiz(questions, students);
+    let scoreforStudent10 = quiz.scoreStudentBySid(10);
     console.log(scoreforStudent10);
 
-    letscoreforStudent11 = quiz.scoreStudentBySid(11);
+    let scoreforStudent11 = quiz.scoreStudentBySid(11);
     console.log(scoreforStudent11);
 
-    letaverage = quiz.getAverageScore();
+    let average = quiz.getAverageScore();
     console.log('Average is: ' + average); //printing the avg
 
 }
